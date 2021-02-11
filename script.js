38fe393594e9868460c81a3392f21e11e08ee93a
SEKI-YUTA <zard.since1991@gmail.com>
2021-02-11 21:13:04 +0900

    mouseStorker added

---
+++ script.js
@@ -0,0 +1,37 @@
+'use strict';
+
+
+
+{
+    const $circle = document.getElementById('js_circle');
+    const $slide = document.getElementById('js_places_card_slide');
+
+  let hovering = false;
+
+    $slide.addEventListener('mousemove', (event) => {
+        // pointerX = event.pageX;
+        // pointerY = event.pageY;
+        hovering = true;
+        $circle.style.transform = 'scale(1)';
+        // circle.style.left = (pointerX - 50) + 'px';
+        // circle.style.top = (pointerY - 50) + 'px';
+        // console.log('event.pageX' + event.pageX)
+        // console.log('event.pageY' + event.pageY)
+        // changePos();
+        if(hovering) {
+          $circle.style.left = event.clientX + 'px';
+          $circle.style.top = event.clientY + 'px';
+        }
+        // $circle.style.transform = 'scale(0)';
+    });
+    $slide.addEventListener('mouseout',(event) => {
+      $circle.style.transform = 'scale(0)';
+      hovering = false;
+    })
+    window.addEventListener('scroll',() => {
+      $circle.style.transform = 'scale(0)';
+      hovering = false;
+    })
+
+    // setInterval(changePos, 20);
+}
